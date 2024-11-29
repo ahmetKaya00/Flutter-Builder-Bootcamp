7ef377a8b3fd7013f1f2307d2260a47c1b862af6
Ahmet Kaya <115210242+ahmetKaya00@users.noreply.github.com>
2024-11-29 20:01:12 +0300

    tema ve dil işlemleri

---
+++ firebase/android/app/src/main/kotlin/com/firabase/firebase/firebase/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.firabase.firebase.firebase
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ firebase/lib/screens/profile_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'Profil',
+        style: TextStyle(fontSize: 24),
+      ),
+    );
+
+  }
+}
